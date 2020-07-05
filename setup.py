722dc2565e0e93e48c7f209d9e1c8e85c166fdee
buerger-pascal <47589869+buerger-pascal@users.noreply.github.com>
2020-07-05 16:04:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['one_pkg'],
+    packages=['visy_neopixel_pkg'],
     package_dir={'': 'scripts'}
     )
 
